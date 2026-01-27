ef73aa87060c31b71d9b1029adad4897b091ff6a
pete <pete@hurth.net>
2026-01-26 19:29:48 -0600

    chore: bump version to 2.6.0-pre1 (versionCode 51)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.blemqttbridge"
         minSdk = 26  // Android 8.0
         targetSdk = 34
-        versionCode = 50
-        versionName = "2.5.17"
+        versionCode = 51
+        versionName = "2.6.0-pre1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
